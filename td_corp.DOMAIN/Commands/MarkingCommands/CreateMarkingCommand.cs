a6a1ff3abb0aef55f8a56208e7b90d04afddaa29
Jonathas Moura <jonathas_eletronica@hotmail.com>
2020-06-08 12:06:00 -0300

    Implementação de classe CreateMarkingCommand

--- td_corp.DOMAIN/Commands/MarkingCommands/CreateMarkingCommand.cs
+++ td_corp.DOMAIN/Commands/MarkingCommands/CreateMarkingCommand.cs
@@ -1,8 +1,23 @@
 using System;
+using Flunt.Notifications;
+using Flunt.Validations;
+using td_corp.SHARED;
 
 namespace td_corp.DOMAIN.Commands.MarkingCommands
 {
-    public class CreateMarkingCommand
+    public class CreateMarkingCommand : Notifiable, ICommand
     {
+        public CreateMarkingCommand(){}
+        public CreateMarkingCommand(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+        public void Validate()
+        {
+            AddNotifications(new Contract()
+            .Requires().HasMinLen(Name, 3, "RegistrarMarcaCommand.Name", "Nome deve conter no mínimo 3 caracteres")
+            .HasMaxLen(Name, 140, "RegistrarMarcaCommand.Name", "Nome deve conter no máximo 140 caracteres"));
+        }
     }
 }
