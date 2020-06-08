aea7f7ea8829bb7b0f21262e45b0d31f7a7d8d44
Jonathas Moura <jonathas_eletronica@hotmail.com>
2020-06-08 19:04:21 -0300

    Implementação dos métodos.

--- td_corp.INFRA/Repositories/MarkingRepository.cs
+++ td_corp.INFRA/Repositories/MarkingRepository.cs
@@ -1,35 +1,53 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using td_corp.DOMAIN.Entities;
 using td_corp.DOMAIN.Repositories;
+using td_corp.INFRA.DataContext;
 
 namespace td_corp.INFRA.Repositories
 {
     public class MarkingRepository : IMarkingRepository
     {
+        private readonly CorpContext _context;
+        public MarkingRepository(CorpContext context)
+        {
+            _context = context;
+        }
         public void CreateMarking(Marking marca)
         {
-            throw new NotImplementedException();
+            _context.Markings.Add(marca);
+            _context.SaveChanges();
         }
 
         public IEnumerable<Marking> GetAll()
         {
-            throw new NotImplementedException();
+            var all = _context.Markings
+            .AsNoTracking()
+            .Where(x => x.IsActive == true)
+            .OrderBy(x => x.Name);
+
+            return all;
         }
 
         public Marking GetById(Guid id)
         {
-            throw new NotImplementedException();
+            var mark = _context.Markings
+            .FirstOrDefault(x => x.Id == id);
+            return mark;
         }
 
         public bool MarkingExists(string mark)
         {
-            throw new NotImplementedException();
+            var sql = _context.Markings.Any(x => x.Name == mark);
+            return sql;
         }
 
         public void UpdateMarking(Marking entity)
         {
-            throw new NotImplementedException();
+            _context.Entry<Marking>(entity).State = EntityState.Modified;
+            _context.SaveChanges();
         }
     }
 }
