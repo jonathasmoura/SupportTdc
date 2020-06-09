dcc9ce64b3b48b2dfed0ce4a4187994394a17dba
Jonathas Moura <jonathas_eletronica@hotmail.com>
2020-06-09 11:58:04 -0300

    Criação de endpoints e instalação de pacotes /refs

---
+++ td_corp.API/Controllers/MarkingController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using td_corp.DOMAIN.Commands.MarkingCommands;
+using td_corp.DOMAIN.CommandsResults;
+using td_corp.DOMAIN.Entities;
+using td_corp.DOMAIN.MarkingCommandHandlers;
+using td_corp.DOMAIN.Repositories;
+
+namespace td_corp.API.Controllers
+{
+    [ApiController]
+    [Route("v1/marcas")]
+    public class MarkingController : ControllerBase
+    {
+        [Route("")]
+        [HttpGet]
+        public IEnumerable<Marking> GetAllMarkings([FromServices] IMarkingRepository marckingRepository)
+        {
+            return marckingRepository.GetAll();
+        }
+
+        [Route("")]
+        [HttpPost]
+        public CommandsResult CreateMarking([FromBody] CreateMarkingCommand command, [FromServices] MarkingCommandHandler handler)
+        {
+            return (CommandsResult)handler.Handle(command);
+        }
+    }
+}
